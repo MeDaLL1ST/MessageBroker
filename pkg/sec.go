940a24af2249b97585e5a1bb7089864e98ff1364
KORABLEV <63864439+MeDaLL1ST@users.noreply.github.com>
2024-08-30 03:30:05 +0300

    Update sec.go

--- pkg/sec.go
+++ pkg/sec.go
@@ -1,20 +1,8 @@
 package pkg
 
 import (
-	"crypto/sha256"
-	"fmt"
 	"os"
 )
-
-func generateChecksum(inhash, key, value string) bool {
-	data := []byte(key + value + os.Getenv("API_KEY"))
-	hash := sha256.Sum256(data)
-	if inhash == fmt.Sprintf("%x", hash[:]) {
-		return true
-	} else {
-		return false
-	}
-}
 
 func validateToken(token string) bool {
 	if token == os.Getenv("API_KEY") {
